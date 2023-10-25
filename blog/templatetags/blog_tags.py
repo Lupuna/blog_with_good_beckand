f8fd07d38fbec14e6fdd070ce64ebac4307ac6c3
Lupuna <terragod427@gmail.com>
2023-10-25 20:20:25 +0700

    news feeds and sitemap

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 from .. import models
 
 register = template.Library()
@@ -9,9 +12,21 @@ def total_posts():
     return models.Post.published.count()
 
 
-@register.inclusion_tag('blog/post/includes/latests_posts.html')
+@register.inclusion_tag('blog/post/includes/show_latest_post.html')
 def show_latest_post(count=5):
-    latest_posts = models.Post.order_by('-publish')[:count]
+    latest_posts = models.Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    max_comment = models.Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+    return max_comment
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    # mark_safe помечает информацию как безопасная (т.е. переданный HTML код не будет экранироваться)
+    return mark_safe(markdown.markdown(text))
 
 # В этом файле хранятся кастомные фильтры и теги для django template language
